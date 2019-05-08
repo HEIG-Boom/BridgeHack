71338c6a10852a3b0f2a47ed0666e5ac829135c5
Laykel <layke.luc@gmail.com>
2019-05-08 17:29:24 +0200

    Setup maven

---
+++ src/main/java/ch/heigvd/mcr/bridgehack/App.java
@@ -0,0 +1,7 @@
+package ch.heigvd.mcr.bridgehack;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Mordor");
+    }
+}
---
+++ src/test/java/ch/heigvd/mcr/bridgehack/AppTest.java
@@ -0,0 +1,12 @@
+package ch.heigvd.mcr.bridgehack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    public void testShouldTest() {
+        assertEquals(21, 20 + 1);
+    }
+}
