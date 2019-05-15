3b65ffcf06d547f1535b0e98647bca52369d8189
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-05-15 17:06:20 +0200

    added a line for fixing the path for lwjgl library

--- src/main/java/ch/heigvd/mcr/bridgehack/App.java
+++ src/main/java/ch/heigvd/mcr/bridgehack/App.java
@@ -1,7 +1,11 @@
 package ch.heigvd.mcr.bridgehack;
 
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
+        System.setProperty("org.lwjgl.librarypath", new File("lib/natives").getAbsolutePath());
+
         System.out.println("Hello Mordor");
     }
 }
