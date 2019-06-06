4a11b1a9139078496816a3c19a6cd065bb3eb8d4
Laykel <layke.luc@gmail.com>
2019-06-07 00:10:50 +0200

    Modified Player and BridgeHack to test new roles

--- src/main/java/ch/heigvd/mcr/bridgehack/BridgeHack.java
+++ src/main/java/ch/heigvd/mcr/bridgehack/BridgeHack.java
@@ -1,11 +1,18 @@
 package ch.heigvd.mcr.bridgehack;
 
 import ch.heigvd.mcr.bridgehack.player.Player;
+import ch.heigvd.mcr.bridgehack.player.races.Dwarf;
+import ch.heigvd.mcr.bridgehack.player.races.Human;
+import ch.heigvd.mcr.bridgehack.player.roles.Hunter;
+import ch.heigvd.mcr.bridgehack.player.roles.Knight;
+import ch.heigvd.mcr.bridgehack.player.roles.Wizard;
 import org.newdawn.slick.*;
 
 public class BridgeHack extends BasicGame {
     private Map map;
     private Player player;
+    // Temporary
+    private Player enemy;
 
     public BridgeHack() throws SlickException {
         super("BridgeHack");
@@ -14,7 +21,9 @@ public class BridgeHack extends BasicGame {
     @Override
     public void init(GameContainer container) throws SlickException {
         map = new Map();
-        player = new Player(map,40, 56);
+        player = new Human(new Knight(), map, 40, 56);
+        // Temporary
+        enemy = new Dwarf(new Wizard(), map, 56, 56);
     }
 
     @Override
@@ -22,20 +31,32 @@ public class BridgeHack extends BasicGame {
         g.scale(2, 2);
         map.render();
         player.render(g);
+        // Temporary
+        enemy.render(g);
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
         player.update(delta);
+        // Temporary
+        enemy.update(delta);
     }
 
     @Override
     public void keyPressed(int key, char c) {
         switch (key) {
-            case Input.KEY_UP:    player.move(0); break;
-            case Input.KEY_LEFT:  player.move(1); break;
-            case Input.KEY_DOWN:  player.move(2); break;
-            case Input.KEY_RIGHT: player.move(3); break;
+            case Input.KEY_UP:
+                player.move(0);
+                break;
+            case Input.KEY_LEFT:
+                player.move(1);
+                break;
+            case Input.KEY_DOWN:
+                player.move(2);
+                break;
+            case Input.KEY_RIGHT:
+                player.move(3);
+                break;
         }
     }
 
--- src/main/java/ch/heigvd/mcr/bridgehack/player/Player.java
+++ src/main/java/ch/heigvd/mcr/bridgehack/player/Player.java
@@ -1,9 +1,16 @@
 package ch.heigvd.mcr.bridgehack.player;
 
 import ch.heigvd.mcr.bridgehack.Map;
+import ch.heigvd.mcr.bridgehack.player.roles.Role;
 import org.newdawn.slick.*;
 
-public class Player {
+/**
+ * Class representing a player
+ */
+public abstract class Player {
+    // Base path to image resources
+    static final private String IMG_BASE_PATH = "/src/main/resources/img/";
+
     private int x;
     private int y;
     private boolean moving = false;
@@ -12,21 +19,27 @@ public class Player {
     private Animation idleAnimation = new Animation();
     private Animation runAnimation = new Animation();
 
+    private Role role;
+
     /**
      * Constructor for the players character.
      *
+     * @param role the initial role of the character
      * @param map a reference to the map for collision detection
      * @param x the x starting coordinate
      * @param y the y starting coordinate
      * @throws SlickException if a problem occurred building the animations
      */
-    public Player(Map map, int x, int y) throws SlickException {
+    public Player(Role role, Map map, int x, int y) throws SlickException {
+        this.role = role;
         this.map = map;
         this.x = x;
         this.y = y;
+
+        String imageBasePath = IMG_BASE_PATH + getBaseImageName();
         for (int i = 0; i < 4; ++i ) {
-            idleAnimation.addFrame(new Image("/src/main/resources/img/knight_m_idle_anim_f" + i + ".png"), 100);
-            runAnimation.addFrame(new Image("/src/main/resources/img/knight_m_run_anim_f" + i + ".png"), 100);
+            idleAnimation.addFrame(new Image(imageBasePath + "_idle_anim_f" + i + ".png"), 100);
+            runAnimation.addFrame(new Image(imageBasePath + "_run_anim_f" + i + ".png"), 100);
         }
     }
 
@@ -75,7 +88,6 @@ public class Player {
             x = (int) Math.floor(x);
             y = (int) Math.floor(y);
         }
-
     }
 
     /**
@@ -92,5 +104,14 @@ public class Player {
         } else {
             g.drawAnimation(idleAnimation, x - 8, y - 24);
         }
+    }
+
+    /**
+     * Get base image name for the instances's role
+     *
+     * @return The base image name for the current role
+     */
+    public String getBaseImageName() {
+        return role.getBaseImageNameImpl();
     }
 }
